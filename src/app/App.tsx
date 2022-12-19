496baebc344899e61860c680eda3587a6b1afb1b
olekbaran <olekbaran24@gmail.com>
2022-12-19 13:19:32 +0100

    chore: cleaning project

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+export const App = () => (
+    <div className='wrapper'>
+        <a href="https://soundise.com" target="_blank">
+            <img src="https://assets.soundise.com/headers/header-github-readme-dark.svg" alt="Soundise React component library" className='image' />
+        </a>
+        <h1 className='heading'>react-components</h1>
+        <p className='description'>Soundise React component library</p>
+    </div>
+)
