cc7809250216ccd8ef341125284e8467fd682c21
olekbaran <olekbaran24@gmail.com>
2022-12-19 20:08:01 +0100

    chore: eslint prettier husky

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 export const App = () => (
-    <div className='wrapper'>
-        <a href="https://soundise.com" target="_blank">
-            <img src="https://assets.soundise.com/headers/header-github-readme-dark.svg" alt="Soundise React component library" className='image' />
-        </a>
-        <h1 className='heading'>react-components</h1>
-        <p className='description'>Soundise React component library</p>
-    </div>
-)
+  <div className="wrapper">
+    <a href="https://soundise.com" target="_blank" rel="noreferrer">
+      <img
+        src="https://assets.soundise.com/headers/header-github-readme-dark.svg"
+        alt="Soundise React component library"
+        className="image"
+      />
+    </a>
+    <h1 className="heading">react-components</h1>
+    <p className="description">Soundise React component library</p>
+  </div>
+);
