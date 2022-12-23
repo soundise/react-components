a9ab3d83816f2d985913c47517b108b68337da29
olekbaran <olekbaran24@gmail.com>
2022-12-23 13:00:50 +0100

    feat: add global style

--- src/components/theme/index.ts
+++ src/components/theme/index.ts
@@ -1,3 +1,4 @@
 export type { ITheme } from './theme';
 export { darkTheme } from './darkTheme';
 export { lightTheme } from './lightTheme';
+export { GlobalStyle } from './GlobalStyle';
--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import { App } from 'app';
-import { darkTheme } from 'components';
-
-export const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap');
-
-    * {
-        box-sizing: border-box;
-        padding: 0;
-        border: 0;
-        margin: 0;
-        font-family: ${({ theme }) => theme.typography.fonts.primary}, ${({
-  theme,
-}) => theme.typography.fonts.secondary}, ${({ theme }) =>
-  theme.typography.fonts.fallback};
-        font-weight: ${({ theme }) => theme.typography.weights.regular};
-        color: ${({ theme }) => theme.typography.colors.primary};
-        text-decoration: none;
-        scroll-behavior: smooth;
-    }
-
-    body {
-        background: ${({ theme }) => theme.colors.background};
-    }
-`;
+import { darkTheme, GlobalStyle } from 'components';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
