5ec267805bf1ef4fee8a15d7b570fd88c33fb560
Olek Baran <olekbaran24@gmail.com>
2022-12-29 12:19:42 +0100

    chore: lib config (#3)
    * chore: better file structure
    
    * chore: lib config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      include: ['src/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@soundise/react-components',
+      fileName: 'soundise-react-components',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'styled-components'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'styled-components': 'Styled Components',
+        },
+      },
+    },
+  },
 });
