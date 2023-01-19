898864b7413a4fd02f0c8716dda63b7d825c2cab
olekbaran <olekbaran24@gmail.com>
2023-01-19 13:33:03 +0100

    feat: addon a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,6 +10,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
     'storybook-addon-rem',
   ],
   framework: {
